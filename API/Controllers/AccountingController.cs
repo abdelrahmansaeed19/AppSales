bb566922e7b9d882f4d8b8e5cf6967770e0e6710
Mostafaragab22 <115711250+Mostafaragab22@users.noreply.github.com>
2026-01-28 14:30:02 +0200

    Add files via upload

--- API/Controllers/AccountingController.cs
+++ API/Controllers/AccountingController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using App_Sales.DTO.AccountingDTO;
 
-namespace API.Modules.Accounting
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/accounting")]
+public class AccountingController : ControllerBase
 {
-    public class AccountingController : Controller
+    private readonly AccountingService _service;
+
+    public AccountingController(AccountingService service)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _service = service;
+    }
+
+    [HttpPost("journal")]
+    public IActionResult CreateJournal(CreateJournalEntryDto dto)
+    {
+        long tenantId = 1;
+        _service.CreateJournalEntry(dto, tenantId);
+        return Ok("Journal Entry Created");
+    }
+
+    [HttpGet("ledger")]
+    public IActionResult GeneralLedger()
+    {
+        long tenantId = 1;
+        return Ok(_service.GeneralLedger(tenantId));
+    }
+
+    [HttpGet("trial-balance")]
+    public IActionResult TrialBalance()
+    {
+        long tenantId = 1;
+        return Ok(_service.TrialBalance(tenantId));
     }
 }
