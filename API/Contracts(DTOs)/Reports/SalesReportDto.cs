c5b55294b834aeb51a10abe1bb54978f4e4cf411
Mostafaragab22 <115711250+Mostafaragab22@users.noreply.github.com>
2026-01-29 12:52:42 +0200

    Add files via upload

---
+++ API/Contracts(DTOs)/Reports/SalesReportDto.cs
@@ -0,0 +1,9 @@
+﻿namespace App_Sales.DTO.ReportDTO
+{
+    public class SalesReportDto
+    {
+        public DateTime Date { get; set; }
+        public decimal TotalSales { get; set; }
+        public int OrdersCount { get; set; }
+    }
+}
